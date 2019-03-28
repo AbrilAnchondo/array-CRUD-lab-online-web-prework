b0036b85a4096dc2fd50d6f8b44e76d118a903a9
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-03-28 02:23:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array.unshift = "dance"
+  my_array.unshift("dance")
 end
 
 def remove_element_from_end_of_array(array)
