7cabc6749e05d8edce03867fe377e9d51892f57a
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-03-28 02:19:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
- my_array = [food, exercise, mindfulness, kindness]
+ my_array = ["nutrition", "exercise", "mindfulness", "kindness"]
 end
 
 def add_element_to_end_of_array(array, element)
